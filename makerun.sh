b3b732fe0cf80315331232b540898a86e9a004f1
postarc <45208608+postarc@users.noreply.github.com>
2019-01-11 17:27:14 +0300

    Update makerun.sh

--- makerun.sh
+++ makerun.sh
@@ -4,8 +4,8 @@
 # Add the following to the crontab (i.e. crontab -e)
 # */1 * * * * ~/masternode/geekcash/makerun.sh
 
-process=geekcashd01
-makerun="geekcashd01"
+process=geekcashd
+makerun="geekcashd"
 
 if ps ax | grep -v grep | grep $process > /dev/null
 then
