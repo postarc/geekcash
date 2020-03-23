59e605a549ed45a06365631465929473a5e5f4f2
postarc <45208608+postarc@users.noreply.github.com>
2020-03-23 21:51:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -86,6 +86,7 @@ else
         sudo bash -c "cp ./$BPATH/geekcashd /usr/bin/"
         sudo bash -c "cp ./$BPATH/geekcash-cli /usr/bin/"
         sudo chmod +x /usr/bin/geekcash*
+	cd ..
         rm -rf temp
 fi 
 
